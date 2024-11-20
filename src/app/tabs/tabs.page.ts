830e091a3397009cf48528e37b691406eff82b1a
Ignacio <ign.cornejoz@duocuc.cl>
2024-11-20 13:08:06 -0300

    V.1.0.1

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ) {}
+
+home(){
+  this.router.navigate(['/tabs/home'], {replaceUrl: true});
+}
+
+servicios(){
+  this.router.navigate(['/tabs/servicios'], {replaceUrl: true});
+}
+historial(){
+  this.router.navigate(['/tabs/historial'], {replaceUrl: true});
+}
+cuenta(){
+  this.router.navigate(['/tabs/cuenta'], {replaceUrl: true});
+}
 
 }
